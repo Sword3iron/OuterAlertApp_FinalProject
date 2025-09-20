2d2ecad0de10dd4fa71c72d6cd122beff2da69bc
Dylan Loke <dylanloke4896@gmail.com>
2025-09-20 13:26:58 +0800

    Main 'App' file Js script

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ThemeProvider from './componentFolder/colorComponent/ThemeContext';
+
+import BottomTabNavigator from './bottomNavigation/BottomNavBar';
+import SplashScreen from './screens/Splash';
+import LoginScreen from './screens/Login';
+import RegisterScreen from './screens/SingUp';
+import ProfilePage from './screens/Profile';
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Splash" component={SplashScreen} screenOptions={{ headerShown: false }}/>
+          <Stack.Screen name="Login" component={LoginScreen} screenOptions={{ headerShown: false }}/>
+          <Stack.Screen name="SignUp" component={RegisterScreen} screenOptions={{ headerShown: false}}/>
+          <Stack.Screen name="HomeTab" component={BottomTabNavigator} screenOptions={{ headerShown: false }}/>
+          <Stack.Screen name="Profile" component={ProfilePage} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+} 
+
